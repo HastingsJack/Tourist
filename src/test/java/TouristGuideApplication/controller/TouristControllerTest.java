e35f50758e50a7437527423f8544a0b55ae0364e
J-AGGERHOLM <jajo0004@stud.kea.dk>
2025-03-14 09:41:23 +0100

    Controller test class generated

---
+++ src/test/java/TouristGuideApplication/controller/TouristControllerTest.java
@@ -0,0 +1,53 @@
+package TouristGuideApplication.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouristControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+
+
+    @Test
+    void getAllAttractions() {
+    }
+
+    @Test
+    void getAttraction() {
+    }
+
+    @Test
+    void addAttraction() {
+    }
+
+    @Test
+    void testAddAttraction() {
+    }
+
+    @Test
+    void editAttractionForm() {
+    }
+
+    @Test
+    void editAttraction() {
+    }
+
+    @Test
+    void deleteAttraction() {
+    }
+
+    @Test
+    void getTagsForAttraction() {
+    }
+}
