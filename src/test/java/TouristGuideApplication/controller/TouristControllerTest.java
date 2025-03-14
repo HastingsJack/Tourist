2528e08805ed901e58f42309612987b9d718de1c
J-AGGERHOLM <jajo0004@stud.kea.dk>
2025-03-14 15:33:06 +0100

    added proper dependency and first draft of test classes

--- src/test/java/TouristGuideApplication/controller/TouristControllerTest.java
+++ src/test/java/TouristGuideApplication/controller/TouristControllerTest.java
@@ -6,12 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(TouristController.class)
@@ -29,6 +36,7 @@ class TouristControllerTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -78,12 +86,43 @@ class TouristControllerTest {
 
     //Jacob//
     @Test
-    void deleteAttraction() {
+    void deleteAttraction() throws Exception {
+
+        //arrange
+        String attractionName = "YellowStone";
+
+        doNothing().when(touristService).deleteAttraction(any());
+
+        // act
+        mockMvc.perform(post("/attractions/delete/" + attractionName))
+                .andExpect(status().is3xxRedirection()) // Redirect expected
+                .andExpect(redirectedUrl("/attractions"));
+
+        // assert
+        verify(touristService, times(1)).deleteAttraction(any());
     }
 
+
     @Test
-    void getTagsForAttraction() {
+    void getTagsForAttraction() throws Exception {
+        //arrange
+        String attractionName = "YellowStone";
+        List<String> mockTags = List.of("Nature", "Adventure", "Hiking");
+
+        when(touristService.getTagsForAttraction(attractionName)).thenReturn(mockTags);
+
+        // act
+        mockMvc.perform(get("/attractions/" + attractionName + "/tags"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tags"))
+                .andExpect(model().attributeExists("tags"))
+                .andExpect(model().attribute("tags", mockTags))
+                .andExpect(model().attribute("name", attractionName));
+
+        // assert
+        verify(touristService, times(1)).getTagsForAttraction(attractionName);
     }
+
 
     //Jacob End//
 }
